bbd0c71d8f2debdd9d249454e2e626a7eda59336
ksaini <kunal_response@yahoo.co.in>
2018-09-18 21:54:29 +0530

    dashboard and product mapping check
    dashboard
    product mapping check for cnfrm orders

--- www/assets/js/demo.js
+++ www/assets/js/demo.js
@@ -1,6 +1,5 @@
 type = ['','info','success','warning','danger'];
 
-
 demo = {
 		initPickColor: function(){
 			$('.pick-class-label').click(function(){
@@ -16,65 +15,18 @@ demo = {
 			});
 		},
 
-		initChartist: function(){    
-
-			var dataSales = {
-					labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
-					series: [
-					         [70, 385, 490, 492, 554, 586, 698, 695, 752, 788, 846, 944],
-					         [67, 152, 143, 240, 287, 335, 435, 437, 539, 542, 544, 647],
-					         [23, 113, 67, 108, 190, 239, 307, 308, 439, 410, 410, 509]
-					         ]
-			};
-
-			var optionsSales = {
-					lineSmooth: false,
-					low: 0,
-					high: 800,
-					showArea: true,
-					height: "245px",
-					axisX: {
-						showGrid: false,
-					},
-					lineSmooth: Chartist.Interpolation.simple({
-						divisor: 3
-					}),
-					showLine: false,
-					showPoint: false,
-			};
-
-			var responsiveSales = [
-			                       ['screen and (max-width: 640px)', {
-			                    	   axisX: {
-			                    		   labelInterpolationFnc: function (value) {
-			                    			   return value[0];
-			                    		   }
-			                    	   }
-			                       }]
-			                       ];
-
-			Chartist.Line('#stressResult', dataSales, optionsSales, responsiveSales);
-
+		initChartist: function(l,s,max){    
 
 			var data = {
-					labels: [ keys[0], keys[1], keys[2], keys[3], keys[4], keys[5], keys[6], keys[7], keys[8], keys[9]],
+					labels: l,
 					series: [
-					         [ barChartResult[keys[0]][0], barChartResult[keys[1]][0], barChartResult[keys[2]][0]
-					         , barChartResult[keys[3]][0], barChartResult[keys[4]][0], barChartResult[keys[5]][0]
-					         , barChartResult[keys[6]][0], barChartResult[keys[7]][0], barChartResult[keys[8]][0]
-					         , barChartResult[keys[9]][0]
-					         ],
-					         [ barChartResult[keys[0]][1], barChartResult[keys[1]][1], barChartResult[keys[2]][1]
-					         , barChartResult[keys[3]][1], barChartResult[keys[4]][1], barChartResult[keys[5]][1]
-					         , barChartResult[keys[6]][1], barChartResult[keys[7]][1], barChartResult[keys[8]][1]
-					         , barChartResult[keys[9]][1]
-					         ]
+					         s
 					        ]
 			};
 
 			var options = {
 					seriesBarDistance: 10,
-					high: 800,
+					high: max,
 					axisX: {
 						showGrid: false
 					},
@@ -85,16 +37,13 @@ demo = {
 			var responsiveOptions = [
 			                         ['screen and (max-width: 640px)', {
 			                        	 seriesBarDistance: 5,
-			                        	 axisX: {
-			                        		 labelInterpolationFnc: function (value) {
-			                        			 return value[0];
-			                        		 }
-			                        	 }
 			                         }]
 			                         ];
 
 			Chartist.Bar('#chartActivity', data, options, responsiveOptions);
-
+		},
+		
+		initChartist1: function(catChartResult){
 			var dataPreferences = {
 					series: [
 					         [25, 30, 20, 25]
@@ -103,7 +52,7 @@ demo = {
 
 			var optionsPreferences = {
 					donut: true,
-					donutWidth: 40,
+					donutWidth: 60,
 					startAngle: 0,
 					total: 100,
 					showLabel: false,
@@ -111,17 +60,12 @@ demo = {
 						showGrid: false
 					}
 			};
-			
-			Chartist.Pie('#chartPreferences', dataPreferences, optionsPreferences);
-
+					
 			Chartist.Pie('#chartPreferences', {
-
-				labels: [pieChartResult.passPer.toString()+'%',pieChartResult.failPer.toString()+'%',pieChartResult.remainsPer.toString()+'%'],
-				series: [pieChartResult.passPer, pieChartResult.failPer, pieChartResult.remainsPer],
-				//labels: ['10%',+'30%','60%'],
-				//series: [10, 30, 60],
+				labels: [catChartResult.p1,catChartResult.p2,catChartResult.p3,catChartResult.p4],
+				series: [catChartResult.p1,catChartResult.p2,catChartResult.p3,catChartResult.p4],
 			});   
-		},
+		},	
 
 		showNotification: function(from, align){
 			color = Math.floor((Math.random() * 4) + 1);
@@ -139,7 +83,6 @@ demo = {
 				}
 			});
 		}
-
 
 }
 
--- www/js/pieChartValues.js
+++ www/js/pieChartValues.js
@@ -1,139 +1,78 @@
-var pieChartResult={
-		passPer:0,
-		failPer:0,
-		remainsPer:100,
-};
+
 var catChartResult={
-		gen:0,
-		obc:0,
-		sc:0,
-		st:0
+		p1:0,
+		p2:0,
+		p3:0,
+		p4:0
 		
 };
 var barChartResult=null;
 var keys=new Array();
 var chartVal = new Array();
 var chartBase = new Array();
-function getLatestValues() {
-	 
-	var req = new XMLHttpRequest();
-	
-	req.onreadystatechange = function() {
-		if (req.readyState == 4) {
-			// check for HTTP status of OK
-			if (req.status == 200) {
-				try {
-					//alert(req.responseText);
-					var job=JSON.parse(req.responseText);
-					
-					//Set Result for Pie Chart
-					var pieChart=job["pieChart"];
-					male=pieChart["male"];
-					
-					female=pieChart["female"];
-					total=pieChart["total"];
-					
-					pieChartResult.failPer=0;
-					pieChartResult.remainsPer=Math.round((male*100)/total,1);
-					pieChartResult.passPer=Math.round((female*100)/total,1);
-		            
-					//demo.initChartist();
-					//Set Result For Bar Chart 
-					keys= new Array();
-					chartVal = new Array();
-					chartBase = new Array();
-					
-					barChartResult=job["barChart"];
-					//barChartResult=job;
-					for(key in barChartResult){
-						keys.push(key);
-						chartVal.push(barChartResult[key][0]);
-						chartBase.push(barChartResult[key][1]);
-					}
-					//alert(chartVal);
-					var buildidElement=document.getElementById("buildid");
-					buildidElement.innerHTML="Total Strength:- "+ total;
-					
-					demo.initChartist();
-				
-				//// Pie chart for category
-					var catChart=job["catChart"];
-					gen=catChart["general"];
-					sc=catChart["sc"];
-					st=catChart["st"];
-					obc=catChart["obc"];
-					total=catChart["total"];
-					
-					catChartResult.gen=Math.round((gen*100)/total,1);
-					catChartResult.obc=Math.round((obc*100)/total,1);
-					catChartResult.sc=Math.round((sc*100)/total,1);
-					catChartResult.st=Math.round((st*100)/total,1);
-					
-					var totcatElement=document.getElementById("totcat");
-					totcatElement.innerHTML="Total Strength:- "+ total;
-					
-					demo1.initChartist1();
-				} catch (e) {
-					console.log("Exception::-"+e.toString());
-				}
-			}
-		}
-	};
-	var base_url = document.URL.substr(0,document.URL.lastIndexOf('/'));
-	req.open("POST", base_url + "/service.php", true);
-	req.send(null);
-}
 
-function getFeeReport() {
-	 
-	var req = new XMLHttpRequest();
+function getOrderReport(data) {
 	
-	req.onreadystatechange = function() {
-		if (req.readyState == 4) {
-			// check for HTTP status of OK
-			if (req.status == 200) {
 				try {
-					//alert(req.responseText);
-					var job=JSON.parse(req.responseText);
-					
-					
+					//alert(JSON.stringify(data[0]["ordr"]));
 					//Set Result For Pie Chart 
 					keys= new Array();
 					chartVal = new Array();
 					chartBase = new Array();
-					
-					//barChartResult=job["pieChart"];
-					barChartResult=job["pieChart"];
-					//alert(job["total"]);
-					//alert(JSON.stringify(barChartResult));
-					
-					for(key in barChartResult){
-						keys.push(key);
-						
-						chartVal.push(barChartResult[key]);
-						chartBase.push(key);
+										
+					for (var i = 0; i < 10; i++) {
+						chartVal.push(data[i]["ordr"]);
+						chartBase.push(data[i]["mdate"]);
 					}
-					
-					var expElement=document.getElementById("expected");
-					expElement.innerHTML= "Rs. " + Math.round(job["total"],1) + "K";
-					
-					var discountElement=document.getElementById("discount");
-					discountElement.innerHTML= "Rs. " + Math.round(job["discount"],1) ;
-					
-					var actualElement=document.getElementById("actual");
-					actualElement.innerHTML= "Rs. " + Math.round(job["recieved"],1) ;
-				
-				
-					demo.initChartist();
+								
+					var max = Math.max.apply(Math,chartVal); 
+					demo.initChartist(chartBase,chartVal,max);
 				} catch (e) {
 					console.log("Exception::-"+e.toString());
 				}
+			
+}
+function getTopOrders(data){
+	var ordr = {};
+	var array=[];
+	for (var i = 0; i < data.length; i++) {
+		if(data[i]['ordr_p'].length > 4)
+		{	
+			var ordr_p = JSON.parse(data[i]['ordr_p']);
+			
+			for (var j = 0; j < ordr_p.length; j++) {
+			//alert(ordr[ordr_p[j]["item"]]);
+				if(ordr[ordr_p[j]["item"]])
+					ordr[ordr_p[j]["item"]] += parseFloat(ordr_p[j]["qty"]);
+				else
+					ordr[ordr_p[j]["item"]] = parseFloat(ordr_p[j]["qty"]);
 			}
 		}
-	};
-	var base_url = document.URL.substr(0,document.URL.lastIndexOf('/'));
-	req.open("POST", base_url + "/feeservice.php", true);
-	req.send(null);
+	}
+	for(a in ordr){
+		array.push([a,ordr[a]])
+	}
+	array.sort(function(a,b){return a[1] - b[1]});
+	array.reverse();
+	var o=[];
+	o[0]=array[0];
+	o[1]=array[1];
+	o[2]=array[2];
+	o[3]=array[3];
+	
+	
+	catChartResult.p1=o[0][1];
+	catChartResult.p2=o[1][1];
+	catChartResult.p3=o[2][1];
+	catChartResult.p4=o[3][1];
+										
+	demo.initChartist1(catChartResult);
+	document.getElementById("p1").innerHTML = o[0][0];
+	document.getElementById("p2").innerHTML = o[1][0];
+	document.getElementById("p3").innerHTML = o[2][0];
+	document.getElementById("p4").innerHTML = o[3][0];
+	
+	
 }
 
 
